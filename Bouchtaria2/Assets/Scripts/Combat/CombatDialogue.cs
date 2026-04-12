017c413bbcafd47f7c88d59de689b6fa362bf51b
Ali Bennouna Louridi <96294983+Bouchta003@users.noreply.github.com>
2026-04-12 13:03:28 +0200

    ui(dialogue):fade in and out and progressive dialogue

--- Bouchtaria2/Assets/Scripts/Combat/CombatDialogue.cs
+++ Bouchtaria2/Assets/Scripts/Combat/CombatDialogue.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,15 +6,28 @@ using TMPro;
 
 public class CombatDialogue : MonoBehaviour
 {
-    //These will be setup from editor and only one dialoguecutscene is triggered depending on choice.
-    //
+    [Header("Data")]
     [SerializeField] List<DialogueCutscene> Dialogues;
-    [SerializeField] public GameObject UIDialogue;
+
+    [Header("UI")]
+    [SerializeField] CanvasGroup dialogueCanvasGroup;
     [SerializeField] Image AvatarImage;
+    [SerializeField] public GameObject UIDialogue;
     [SerializeField] TextMeshProUGUI DialogueText;
     [SerializeField] TextMeshProUGUI DialogueName;
-    int currentLine = 0; DialogueCutscene currentScene = null;
+
+    [Header("Settings")]
+    [SerializeField] float fadeDuration = 0.3f;
+    [SerializeField] float typingSpeed = 0.03f;
+
+    int currentLine = 0;
+    DialogueCutscene currentScene = null;
+
+    bool isTyping = false;
+    Coroutine typingCoroutine;
+
     public static CombatDialogue Instance;
+
     private void Awake()
     {
         if (Instance != null)
@@ -21,47 +35,125 @@ public class CombatDialogue : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-
         Instance = this;
     }
+
     public void TriggerCutscene(int id)
     {
-        DialogueCutscene scene = Dialogues[id];
-        //Set UI Dialogue as active and hide gamemanager.ui
-        currentScene = scene;
+        currentScene = Dialogues[id];
+        currentLine = 0;
+
         GameManager.Instance.UIparent.SetActive(false);
-        UIDialogue.SetActive(true);
-        AvatarImage.sprite = scene.Lines[0].Avatar;
-        DialogueText.text = scene.Lines[0].Content;
-        DialogueName.text = scene.Lines[0].Name;
+        StartCoroutine(FadeIn());
+
+        DisplayLine();
     }
+
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (!Input.GetMouseButtonDown(0)) return;
+        if (currentScene == null) return;
+
+        if (isTyping)
         {
-            if (currentScene == null) return;
-            currentLine++;
-            if (currentLine < currentScene.Lines.Count)
-            {
-                AvatarImage.sprite = currentScene.Lines[currentLine].Avatar;
-                DialogueText.text = currentScene.Lines[currentLine].Content;
-                DialogueName.text = currentScene.Lines[currentLine].Name;
-            }
-            else //No more lines end of cutscene
-            {
-                currentScene = null;
-                currentLine = 0;
-                GameManager.Instance.UIparent.SetActive(true);
-                UIDialogue.SetActive(false);
-            }
+            // Finish instantly
+            StopCoroutine(typingCoroutine);
+            DialogueText.text = currentScene.Lines[currentLine].Content;
+            isTyping = false;
+        }
+        else
+        {
+            NextLine();
         }
     }
+
+    void NextLine()
+    {
+        currentLine++;
+
+        if (currentLine < currentScene.Lines.Count)
+        {
+            DisplayLine();
+        }
+        else
+        {
+            StartCoroutine(EndDialogue());
+        }
+    }
+
+    void DisplayLine()
+    {
+        var line = currentScene.Lines[currentLine];
+
+        DialogueName.text = line.Name;
+        AvatarImage.sprite = line.Avatar;
+
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        typingCoroutine = StartCoroutine(TypeText(line.Content));
+    }
+
+    IEnumerator TypeText(string text)
+    {
+        isTyping = true;
+        DialogueText.text = "";
+
+        foreach (char c in text)
+        {
+            DialogueText.text += c;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTyping = false;
+    }
+
+    IEnumerator FadeIn()
+    {
+        dialogueCanvasGroup.gameObject.SetActive(true);
+
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            dialogueCanvasGroup.alpha = t / fadeDuration;
+            yield return null;
+        }
+
+        dialogueCanvasGroup.alpha = 1;
+    }
+
+    IEnumerator FadeOut()
+    {
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            dialogueCanvasGroup.alpha = 1 - (t / fadeDuration);
+            yield return null;
+        }
+
+        dialogueCanvasGroup.alpha = 0;
+        dialogueCanvasGroup.gameObject.SetActive(false);
+    }
+
+    IEnumerator EndDialogue()
+    {
+        yield return StartCoroutine(FadeOut());
+
+        currentScene = null;
+        currentLine = 0;
+
+        GameManager.Instance.UIparent.SetActive(true);
+    }
 }
+
 [System.Serializable]
 public class DialogueCutscene
 {
     public List<DialogueLine> Lines;
 }
+
 [System.Serializable]
 public class DialogueLine
 {
